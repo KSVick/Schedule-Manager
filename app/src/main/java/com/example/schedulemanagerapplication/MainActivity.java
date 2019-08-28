8cb879b4704cdd3e2718c51396fafca15e62dc8b
vickk <vick.kanaan.2012@gmail.com>
2019-08-28 09:01:40 +0700

    First Commit

---
+++ app/src/main/java/com/example/schedulemanagerapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.schedulemanagerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent loginIntent = new Intent(this,RegisterActivity.class);
+        startActivity(loginIntent);
+    }
+}
